2614acb12a90edc69d31c0c729f4b60797e423ef
Reem Ahmed <23-101100@students.eui.edu.eg>
2025-11-11 14:12:27 +0200

    hero-section

---
+++ java.js
@@ -0,0 +1,6 @@
+document.getElementById("id1").textContent = "Home";
+document.getElementById("id2").textContent = "Channel";
+document.getElementById("id3").textContent = "Shows";
+document.getElementById("id4").textContent = "Media";
+document.getElementById("id5").textContent = "Log in";
+document.getElementById("id6").textContent = "Dive Into a World of Adventure and Fun!";
